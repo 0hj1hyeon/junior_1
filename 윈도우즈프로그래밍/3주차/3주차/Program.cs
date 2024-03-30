ffb5cd24c9e49b7d8572747ad9df4a206ec314ff
0hj1hyeon <ojh010911@gmail.com>
2024-03-30 21:19:23 +0900

    24.03.30

---
+++ 윈도우즈프로그래밍/3주차/3주차/Program.cs
@@ -0,0 +1,241 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("1000원이하 숫자 입력: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int w500 = n / 500;
+        n %= 500;
+        Console.WriteLine("500원: "+ w500+"개");
+        int w100 = n / 100;
+        n %= 100;
+        Console.WriteLine("100원: " + w100 + "개");
+        int w50 = n / 50;
+        n %= 50;
+        Console.WriteLine("50원: " + w50 + "개");
+        int w10 = n / 10;
+        n %= 10;
+        Console.WriteLine("10원: " + w10 + "개");
+        int w5 = n / 5;
+        n %= 5;
+        Console.WriteLine("5원: " + w5 + "개");
+        int w1 = n / 1;
+        n %= 1;
+        Console.WriteLine("1원: " + w1 + "개");
+
+    }
+}
+
+
+
+  
+
+class solving {
+    void n1s()
+    {
+        int a = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            a += 2 * i;
+        }
+
+
+        int b = 1;
+        for (int i = 1; i <= 10; i++)
+        {
+            b *= i;
+        }
+
+        int result = a + b;
+
+        Console.WriteLine("1번 결과: " + result);
+    }
+    void n2as() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            int jsum = 1;
+            for (int j = 1; j <= i; j++)
+            {
+                jsum *= j;
+            }
+            sum += jsum;
+        }
+
+
+
+
+        Console.WriteLine("2-a번 결과: " + sum);
+    }
+    void n2bs() {
+        double sum = 0.0;
+        for (int i = 1; i <= 10; i++)
+        {
+
+            if (i % 2 == 0)
+                sum -= 1.0 / i;
+            else
+                sum += 1.0 / i;
+        }
+
+        Console.WriteLine("2-b 결과: " + sum);
+    }
+    void n3s() {
+        for (int i = 0; i <= 4; i += 4)
+        {
+            int a = 2 + i; int b = 3 + i; int c = 4 + i; int d = 5 + i;
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.WriteLine(a + "*" + j + "=" + (a * j) + " " + b + "*" + j + "=" + (b * j) + " " + c + "*" + j + "=" + (c * j) + " " + d + "*" + j + "=" + (d * j));
+            }
+        }
+    }
+    void n4s() {
+        Console.WriteLine("100 이하의 소수:");
+
+        for (int i = 2; i <= 100; i++)
+        {
+            bool p = true;
+
+
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    p = false;
+                    break;
+                }
+            }
+
+
+            if (p)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+    void n5s(){
+        Console.WriteLine("500 이하의 완전수:");
+
+        for (int i = 2; i <= 500; i++)
+        {
+            int sum = 0;
+            for (int j = 1; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                    sum += j;
+            }
+
+            if (sum == i)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+    void n6s(){
+        Console.Write("정수 입력: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        string ns = num.ToString();
+        int l = 0;
+        int r = ns.Length - 1;
+        bool p = true;
+
+        while (l < r)
+        {
+            if (ns[l] != ns[r])
+            {
+                p = false;
+                break;
+            }
+            l++;
+            r--;
+        }
+
+        if (p)
+            Console.WriteLine("회문수");
+        else
+            Console.WriteLine("회문수 아님");
+    }
+    void n7s(){
+        Console.Write("암스트롱수: ");
+        for (int i = 100; i <= 500; i++)
+        {
+            int a = i / 100;
+            int j = i % 100;
+            int b = j / 10;
+            int c = j % 10;
+
+            int r = (int)Math.Pow(a, 3) + (int)Math.Pow(b, 3) + (int)Math.Pow(c, 3);
+            if (i == r)
+            {
+                Console.WriteLine(" " + i);
+            }
+
+        }
+    }
+    void n8s(){
+    }
+
+    void n9s(){
+        int n = 5;
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            for (int j = 1; j <= n - i; j++)
+                Console.Write(" ");
+
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+
+            for (int j = 1; j <= n - i; j++)
+                Console.Write(" ");
+
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+    }
+    void n10s(){
+        Console.Write("1000원이하 숫자 입력: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int w500 = n / 500;
+        n %= 500;
+        Console.WriteLine("500원: " + w500 + "개");
+        int w100 = n / 100;
+        n %= 100;
+        Console.WriteLine("100원: " + w100 + "개");
+        int w50 = n / 50;
+        n %= 50;
+        Console.WriteLine("50원: " + w50 + "개");
+        int w10 = n / 10;
+        n %= 10;
+        Console.WriteLine("10원: " + w10 + "개");
+        int w5 = n / 5;
+        n %= 5;
+        Console.WriteLine("5원: " + w5 + "개");
+        int w1 = n / 1;
+        n %= 1;
+        Console.WriteLine("1원: " + w1 + "개");
+    }
+}
---
+++ 윈도우즈프로그래밍/4주차/4주차/Program.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.VisualBasic;
+
+class student {
+    public int sn;
+    public double k;
+    public double eng;
+    public double math;
+
+    public student(int sn, double k, double eng, double math)
+    {
+        this.sn = sn;
+        this.k = k;
+        this.eng = eng;
+        this.math = math;
+    }
+    public double totalp() {
+
+        return k + eng + math;
+    }
+
+    public double aver()
+    {
+
+        return (k + eng + math)/3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        student[] a = new student[5];
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine($"학생 {i + 1}의 정보를 입력하세요.");
+            Console.Write("학번: ");
+            int sn = int.Parse(Console.ReadLine());
+            Console.Write("국어 성적: ");
+            double k = double.Parse(Console.ReadLine());
+            Console.Write("영어 성적: ");
+            double eng = double.Parse(Console.ReadLine());
+            Console.Write("수학 성적: ");
+            double math = double.Parse(Console.ReadLine());
+
+            
+            a[i] = new student(sn, k, eng, math);
+        }
+
+        double tk =0 ;
+        double teng =0;
+        double tmath=0;
+
+        Console.WriteLine("학생 정보");
+        foreach (student s in a) {
+            Console.WriteLine($"학번:{s.sn}, 총점: {s.totalp()}, 평균: {s.aver()}" );
+            tk += s.k;
+            teng += s.eng;
+            tmath += s.math;
+            
+
+        }
+        tk /= 5;
+        teng /= 5;
+        tmath /= 5;
+        Console.WriteLine($"국어 평균: {tk}, 영어 평균: {teng}, 수학 평균: {tmath}");
+
+    }
+}
+
+
+
+
+
+
+
+class n1 {
+
+    int[,] transpose(int[,] a)
+    {
+        int r = a.GetLength(0);
+        int c = a.GetLength(1);
+
+        int[,] transa = new int[c, r];
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                transa[j, i] = a[i, j];
+            }
+        }
+
+        return transa;
+
+    }
+
+    int[,] a = { { 1, 2, 3 },
+                      { 4, 5, 6 } };
+}
+
+class n2 {
+
+    /*char[] com = { 'C', 'o', 'm', 'p', 'u', 't', 'e', 'r' };
+    foreach (char c in com)
+    {
+        Console.Write(c);
+    }*/
+}
+class n3
+{
+    /*
+     int[][] arr = new int[3][];
+arr[0] = new int[] { 0,1,2};
+arr[1] = new int[] { 4,5,6,7};
+arr[2] = new int[] { 10,11,12,13,14};
+
+
+for (int i = 0; i <= 2; i++) {
+    Console.Write("arr[{0}]: ",i);
+    for (int j = 0; j < arr[i].Length; j++) { 
+        Console.Write("{0} ", arr[i][j]);
+    }
+    Console.WriteLine();
+}
+     */
+}
+class n4
+{
+    /*
+     Random r = new Random();
+
+int[] a = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    a[i] = r.Next(99) + 1;
+}
+
+Array.Sort(a);
+Array.Reverse(a);
+
+
+Console.WriteLine("랜덤정수 내림차순 출력");
+
+
+foreach (int value in a) {
+    Console.Write("{0,5}",value);
+}
+     */
+}
+
+class n5
+{
+    /*
+     Random r = new Random();
+
+double[] a = new double[5];
+
+for (int i = 0; i < 5; i++)
+{
+    a[i] = r.NextDouble() *100;
+}
+
+Array.Sort(a);
+Array.Reverse(a);
+
+
+Console.WriteLine("랜덤정수 내림차순 출력");
+
+
+foreach (double value in a) {
+    Console.Write("{0,5} ",value);
+}
+     */
+}
+
+class n6
+{
+    /*
+     */
+}
+
+class n7
+{
+    /*
+     */
+}
+
+class n8
+{
+    /*
+     */
+}
